758414df1c21f7122e8e8ee8cfc2e72dfb138166
EstefaniaFranco <estefaniafranco20@hotmail.com>
2020-12-01 18:34:50 -0300

    k

--- index.php
+++ index.php
@@ -38,8 +38,8 @@ $app->group('/empleado', function (RouteCollectorProxy $group) {
 // ABM mesas
 $app->group('/mesa', function (RouteCollectorProxy $group) {   
     $group->post('[/]', MesasController::class . ':addOne');
-    $group->delete('/{id}[/]', MesasController::class . ':deleteOne');
-    $group->put('/{id}[/]', MesasController::class . ':update' );
+    //$group->delete('/{id}[/]', MesasController::class . ':deleteOne');
+    $group->post('/{id}[/]', MesasController::class . ':update' );
 })->add(new IsAdminMiddleware)->add(new ValidarTokenMiddleware)->add(new JsonMiddleware);
 
 
